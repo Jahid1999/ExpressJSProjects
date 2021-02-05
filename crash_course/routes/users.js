9fb9a643093c73cb7f8b3ea9fae2c4141169ac68
Jahid1999 <bsse1030@iit.du.ac.bd>
2021-02-05 22:57:44 +0600

    added individual file for routes

--- crash_course/routes/users.js
+++ crash_course/routes/users.js
@@ -1,30 +1,22 @@
 const express = require('express');
-const path = require('path');
-const users = require('./Users');
+const router = express.Router();
+const users = require('../Users');
 
 
-const app = express();
 
-const logger = (req, res, next) => {
-    console.log(`${req.protocol}://${req.get('host')}${req.originalUrl}}`);
-    next();
-};
-
-app.use(logger);
-
-app.get('/', (req, res) => {
+router.get('/', (req, res) => {
     res.send('Hello Express');
 })
 
-app.get('/api/file', (req, res) => {
+router.get('/file', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'home.html'));
 })
 
-app.get('/api/users', (req, res) => {
+router.get('/users', (req, res) => {
     res.status(200).json(users);
 })
 
-app.get('/api/users/:user_id', (req, res) => {
+router.get('/users/:user_id', (req, res) => {
     const found = users.some(user => user.id === parseInt(req.params.user_id));
 
     if(found) {
@@ -35,6 +27,4 @@ app.get('/api/users/:user_id', (req, res) => {
     }
 })
 
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT);
+module.exports = router;
