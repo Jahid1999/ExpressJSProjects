f630c2bd05ee9ba8710faf5c47562809fc5f643e
Jahid1999 <bsse1030@iit.du.ac.bd>
2021-02-05 22:32:02 +0600

    added middleware

---
+++ crash_course/Users.js
@@ -0,0 +1,19 @@
+const users = [
+    {
+        id: 1,
+        name: "Jahid",
+        email: "jahid@gmail.com"
+    },
+    {
+        id: 2,
+        name: "Afreen",
+        email: "afreen@gmail.com"
+    },
+    {
+        id: 3,
+        name: "Diya",
+        email: "afreen@gmail.com"
+    }
+]
+
+module.exports = users;
--- crash_course/index.js
+++ crash_course/index.js
@@ -1,11 +1,23 @@
 const express = require('express');
+const path = require('path');
+const users = require('./Users');
+
 
 const app = express();
 
+
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {console.log('hi')});
+app.listen(PORT);
 
-// app.get('/', (req, res) => {
-//     res.send('Hello Express');
-// })
+app.get('/', (req, res) => {
+    res.send('Hello Express');
+})
+
+app.get('/api/file', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'home.html'));
+})
+
+app.get('/api/users', (req, res) => {
+    res.status(200).json(users);
+})
